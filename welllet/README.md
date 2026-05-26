f3a0a8807ab82ebfe7624c95a0bd0a54867b6abe
Yussof Hassan Mahmoud Abdelmoneim <42410535.Yussof@acu.edu.eg>
2026-05-26 15:55:33 +0300

    Added README and database script

---
+++ welllet/README.md
@@ -0,0 +1,21 @@
+﻿# Digital Wallet System
+
+Wallet System built using:
+
+- C#
+- WinForms
+- SQL Server
+
+## Features
+
+- Register
+- Login
+- Add Money
+- Send Money
+- Transaction History
+
+## How to Run
+
+1. Run database.sql
+2. Open the project in Visual Studio
+3. Run the application
---
+++ welllet/database.sql
@@ -0,0 +1,31 @@
+﻿CREATE TABLE Users
+(
+    UserID INT PRIMARY KEY IDENTITY,
+
+    FullName NVARCHAR(100) NOT NULL,
+
+    PhoneNumber NVARCHAR(11) NOT NULL,
+
+    Password NVARCHAR(100) NOT NULL,
+
+    Balance DECIMAL(10,2) NOT NULL,
+
+    Gender NVARCHAR(10) NOT NULL,
+
+    BirthDate DATE NOT NULL
+);
+
+CREATE TABLE Transactions
+(
+    TransactionID INT PRIMARY KEY IDENTITY,
+
+    SenderID INT NULL,
+
+    ReceiverID INT NOT NULL,
+
+    Amount DECIMAL(10,2) NOT NULL,
+
+    TransactionType NVARCHAR(50) NOT NULL,
+
+    TransactionDate DATETIME NOT NULL
+);
